f12a8273e0eda3d96b9280812a3f16f5d59a2cf6
avelino <avelino5872@gmail.com>
2017-02-13 22:38:40 +0100

    La aplicación abre la cámara cuando se inicia.

--- app/src/main/java/gal/udc/evilcorp/lookaround/MainActivity.java
+++ app/src/main/java/gal/udc/evilcorp/lookaround/MainActivity.java
@@ -1,13 +1,42 @@
 package gal.udc.evilcorp.lookaround;
 
+import android.hardware.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Camera mCamera = null;
+    private CameraView mCameraView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        try{
+            mCamera = Camera.open();//you can use open(int) to use different cameras
+        } catch (Exception e){
+            Log.d("ERROR", "Failed to get camera: " + e.getMessage());
+        }
+
+        if(mCamera != null) {
+            mCameraView = new CameraView(this, mCamera);//create a SurfaceView to show camera data
+            FrameLayout camera_view = (FrameLayout)findViewById(R.id.camera_view);
+            camera_view.addView(mCameraView);//add the SurfaceView to the layout
+        }
+
+        //btn to close the application
+        ImageButton imgClose = (ImageButton)findViewById(R.id.imgClose);
+        imgClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.exit(0);
+            }
+        });
     }
+
 }
